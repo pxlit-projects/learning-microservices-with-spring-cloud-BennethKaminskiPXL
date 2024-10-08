966585d41fafb0b181376ee045ecec8623c10e90
BennethKaminskiPXL <118831756+BennethKaminskiPXL@users.noreply.github.com>
2024-10-08 16:44:32 +0200

    add architecture, add backend : productcatalog-service, shoppingcart-service

---
+++ backend-java/DuurzaamShopPlatform/productcatalog-service/src/main/java/be/pxl/services/catalog/ProductCatalogApp.java
@@ -0,0 +1,17 @@
+package be.pxl.services.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * productcatalog-serviceApp
+ *
+ */
+@SpringBootApplication
+public class ProductCatalogApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ProductCatalogApp.class, args);
+    }
+}
---
+++ backend-java/DuurzaamShopPlatform/shoppingcart-service/src/main/java/be/pxl/services/cart/ShoppingCartServiceApp.java
@@ -0,0 +1,17 @@
+package be.pxl.services.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * shoppingcart service
+ *
+ */
+@SpringBootApplication
+public class ShoppingCartServiceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ShoppingCartServiceApp.class, args);
+    }
+}
